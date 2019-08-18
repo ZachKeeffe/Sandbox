2ba26a2f13a8ab7976b759fb6e6e60bcdd4ab447
Zachary Keeffe <zachary.keeffe@my.jcu.edu.au>
2019-08-18 15:45:28 +1000

    Add Password Check Program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,14 @@
 """Zachary Keeffe"""
+PASSWORD_LENGTH_MINIMUM = 4
+
+
+def main():
+    password = ""
+    while len(password) < PASSWORD_LENGTH_MINIMUM:
+        password = input("Please enter a password: ")
+        if len(password) < PASSWORD_LENGTH_MINIMUM:
+            print("Insufficient Password Length.")
+    print("Entry: ", "*" * len(password))
+
+
+main()
